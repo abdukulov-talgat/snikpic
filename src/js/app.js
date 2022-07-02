835f90744b05ea7624177d52ad8234c914e8eb1b
Abdukulov Talgat <abdukulov.talgat@gmail.com>
2022-07-02 14:54:43 +0500

    Добавляет секцию со слайдером

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,16 @@ burger.addEventListener('click', (evt) => {
     evt.target.closest('.menu__burger').classList.toggle('menu__burger--open');
     document.documentElement.classList.toggle('menu-open');
 });
+
+
+const swiper = new Swiper(document.querySelector('.swiper'), {
+    // loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        type: 'bullets',
+        el: '.swiper-pagination',
+    }
+});
